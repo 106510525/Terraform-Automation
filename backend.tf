b5d60822d1fc820e0216d698d799a6ddc4cdc247
106510525 <54837247+106510525@users.noreply.github.com>
2023-10-29 19:44:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-bucket-shubham-n"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "shubham-dynamodb-table"
+    dynamodb_table = "shubham-dynamodb-table-1"
   }
 }
